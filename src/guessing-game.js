3f6116ddf52e65020dcdfe87ce9e9b13604a746c
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2017-09-29 20:33:12 +0300

    rev1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,19 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
-
-    }
-
-    guess() {
-    return 409;
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+setRange(min, max) {
+  this.Min=min;
+  this.Max=max;
 }
+guess() {
+return Math.round((this.Min+this.Max)/2); //-1)/2;
 
+}
+lower() {
+  this.Max=this.guess();
+}
+greater() {
+  this.Min=this.guess();
+}
+}
 module.exports = GuessingGame;
